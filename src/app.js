d84bf86b236cf467b47f71af7c5110a5de090367
Piotrek <Piotr.karys96@gmail.com>
2024-09-19 20:03:58 +0200

    MONGOdb

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 //apka express rauty middleware
+
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Serwer działa!");
+});
+
+module.exports = app;
--- src/server.js
+++
@@ -1,1 +0,0 @@
-//uruchamianie serwera
