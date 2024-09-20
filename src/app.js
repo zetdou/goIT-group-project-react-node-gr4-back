f052ccc662e6940f9eb3cb78c9e4cfa0b82322d6
Piotrek <Piotr.karys96@gmail.com>
2024-09-20 19:20:26 +0200

    register feature

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorMiddleware");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -12,6 +13,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth", authRoutes);
 app.use(errorHandler);
 
 module.exports = app;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,1 +1,8 @@
 //obsługa /auth/
+const express = require("express");
+const router = express.Router();
+const { signup } = require("../controller/authentication/registerController");
+
+router.post("/register", signup);
+
+module.exports = router;
